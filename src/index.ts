f56df99fd925eb300dc3a879a926fb9187c38def
FedorSelitsky <FedorSelitsky@gmail.com>
2019-02-02 22:27:39 +0300

    Fix indents

--- src/index.ts
+++ src/index.ts
@@ -5,37 +5,37 @@ import App from './App.vue';
 Vue.use(VueRouter);
 
 const routes = [
-	{
-		icon: 'bars',
-		path: '/',
-		name: 'home',
-		title: 'Home',
-	},
-	{
-		icon: 'portrait',
-		path: '/artists',
-		name: 'artists',
-		title: 'Artists',
-	},
-	{
-		icon: 'calendar-check',
-		path: '/events',
-		name: 'events',
-		title: 'Events',
-	},
-	{
-		icon: 'map-marker-alt',
-		path: '/venues',
-		name: 'venues',
-		title: 'Venues',
-	},
+  {
+    icon: 'bars',
+    path: '/',
+    name: 'home',
+    title: 'Home',
+  },
+  {
+    icon: 'portrait',
+    path: '/artists',
+    name: 'artists',
+    title: 'Artists',
+  },
+  {
+    icon: 'calendar-check',
+    path: '/events',
+    name: 'events',
+    title: 'Events',
+  },
+  {
+    icon: 'map-marker-alt',
+    path: '/venues',
+    name: 'venues',
+    title: 'Venues',
+  },
 ];
 
 const router = new VueRouter({
-	routes,
+  routes,
 });
 
 const app = new Vue({
-	router,
-	render: h => h(App),
+  router,
+  render: h => h(App),
 }).$mount('#app');
