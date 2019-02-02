57670d13b5652ca5e7429318c08f2e6926788655
FedorSelitsky <FedorSelitsky@gmail.com>
2019-02-02 22:07:47 +0300

    Create Vue project

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app" class="app dk">
+    <div id="aside" class="app-aside modal fade nav-dropdown">
+      <div class="left navside white dk" data-layout="column">
+        <div class="navbar no-radius">
+          <router-link to="/" class="navbar-brand md">
+            <img src="assets/images/logo.png">
+          </router-link>
+        </div>
+        <div data-flex class="hide-scroll">
+          <nav class="scroll nav-stacked nav-active-primary">
+            <ul id="change_menu" class="nav" data-ui-nav>
+              <li v-for="route in $router.options.routes" :key="route.name">
+                <router-link :to="route.path">
+                  <span class="nav-icon">
+                    <font-awesome-icon v-bind:icon="route.icon"/>
+                  </span>
+                  <span class="nav-text">{{ route.title }}</span>
+                </router-link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faBars,
+  faCalendarCheck,
+  faMapMarkerAlt,
+  faPortrait
+} from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faBars);
+library.add(faCalendarCheck);
+library.add(faMapMarkerAlt);
+library.add(faPortrait);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
+
+@Component
+export default class App extends Vue {}
+</script>
+
+<style>
+@import "assets/styles/app.css";
+@import "assets/styles/font.css";
+</style>
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+	{
+		icon: 'bars',
+		path: '/',
+		name: 'home',
+		title: 'Home',
+	},
+	{
+		icon: 'portrait',
+		path: '/artists',
+		name: 'artists',
+		title: 'Artists',
+	},
+	{
+		icon: 'calendar-check',
+		path: '/events',
+		name: 'events',
+		title: 'Events',
+	},
+	{
+		icon: 'map-marker-alt',
+		path: '/venues',
+		name: 'venues',
+		title: 'Venues',
+	},
+];
+
+const router = new VueRouter({
+	routes,
+});
+
+const app = new Vue({
+	router,
+	render: h => h(App),
+}).$mount('#app');
